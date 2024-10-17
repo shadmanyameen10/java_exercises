ac48a0dee25d8c7ea0820c9d6cc420cd8696ca70
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-17 00:41:34 -0400

    Added solution for Exercise 3

---
+++ src/exercise3.java
@@ -0,0 +1,15 @@
+//Write a Java program to divide two numbers and print them on the screen.
+public class exercise3 {
+
+	public static void main(String[] args) {
+
+		int num1 = 74;
+		int num2 = 36;
+		int quo  = num1 / num2;
+		
+		System.out.println(num1 + " / " + num2 + " = " + quo );
+		
+		
+	}
+
+}
