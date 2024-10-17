a2e8cdcfdc0ccf6056e79af6a29113fec33e17e0
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-17 01:49:04 -0400

    Added solution for Exercise 4

---
+++ src/exercise4.java
@@ -0,0 +1,14 @@
+//Write a Java program to print the results of the following operations.
+
+public class exercise4 {
+
+	public static void main(String[] args) {
+
+		System.out.println(-5 + 8 * 6);
+		System.out.println((55 + 9) % 9);
+		System.out.println(20 + -3 * 5 / 8);
+		System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+	}
+
+}
