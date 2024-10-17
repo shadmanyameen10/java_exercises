cb6ba3fa19f1bcfaa46dec16b7b2a275322c479a
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-16 23:25:47 -0400

    Add solution for Exercise 2

---
+++ src/exercise2.java
@@ -0,0 +1,13 @@
+
+public class exercise2 {
+
+	public static void main(String[] args) {
+		int num1 = 74;
+		int num2 = 36;
+		int sum  = num1 + num2;
+		
+		System.out.println(num1 + " + " + num2 + " = " + sum );
+
+	}
+
+}
