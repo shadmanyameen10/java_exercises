3e1c80f3b98f54efa7aaf7df37940cb4dfa88724
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-20 01:11:33 -0400

    Added solution for Exercise 13

---
+++ src/exercise13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercise13 {
+
+	public static void main(String[] args) {
+
+		double width, height, area, perimeter;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter width: ");
+		width = input.nextDouble();
+
+		System.out.print("Enter height: ");
+		height = input.nextDouble();
+
+		area = width * height;
+		perimeter = 2 * (width + height);
+
+		System.out.println("Area is: " + width + " * " + height + " = " + area);
+		System.out.println("Perimeter is: " + "2 " + " * " + "(" + width + " + " + height + ")" + " = " + perimeter);
+
+		input.close();
+
+	}
+
+}
