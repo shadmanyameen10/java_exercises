b94269ab321bcb6220ef5218cd5a0b9db0df1e73
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-21 19:37:36 -0400

    Added solution for Exercise 21

---
+++ src/exercise21.java
@@ -0,0 +1,22 @@
+// Write a Java program to convert a decimal number to an octal number.
+
+import java.util.Scanner;
+
+public class exercise21 {
+
+	public static void main(String[] args) {
+
+		int dec;
+		String octal;
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a decimal number: ");
+		dec = input.nextInt();
+		octal = Integer.toOctalString(dec);
+		System.out.println(octal);
+
+		input.close();
+
+	}
+
+}
