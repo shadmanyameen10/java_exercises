b1a5ea213cc6e72e721562f7445e6ab698252a83
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-21 21:21:18 -0400

    Added and modified solution for Exercise 22

---
+++ src/exercise22.java
@@ -0,0 +1,25 @@
+// Write a Java program to convert a binary number to a decimal number.
+
+import java.util.Scanner;
+
+public class exercise22 {
+
+	public static void main(String[] args) {
+
+		String binary;
+		int decimal;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a binary value: ");
+		binary = input.nextLine();
+
+		decimal = Integer.parseInt(binary, 2);
+
+		System.out.println("Decimal Number: " + decimal);
+
+		input.close();
+
+	}
+
+}
