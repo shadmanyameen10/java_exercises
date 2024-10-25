db2c662b6e05c93359369e0035f6e0da75f06378
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-24 23:34:07 -0400

    Added solution for Exercise 31

---
+++ src/exercise31.java
@@ -0,0 +1,17 @@
+//Write a Java program to check whether Java is installed on your computer.
+
+public class exercise31 {
+
+	public static void main(String[] args) {
+
+	    System.out.println(System.getProperty("java.version"));
+	    System.out.println(System.getProperty("java.runtime.version"));
+	    System.out.println(System.getProperty("java.home"));
+	    System.out.println(System.getProperty("java.vendor"));
+	    System.out.println(System.getProperty("java.vendor.url"));
+	    System.out.println(System.getProperty("java.class.path"));
+
+		
+	}
+
+}
