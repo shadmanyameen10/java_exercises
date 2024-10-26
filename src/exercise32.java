3f9bd35a767921954f66c26db12cd01384a7a14a
Shadman Yameen <shadmanyameen10@gmail.com>
2024-10-26 00:54:00 -0400

    Added and formatted solution for Exercise 32

--- src/exercise32.java
+++ src/exercise32.java
@@ -1,33 +1,34 @@
 // Write a Java program to compare two numbers.
 
 import java.util.Scanner;
+
 public class exercise32 {
 
 	public static void main(String[] args) {
 
-		int num1,num2;
-		
+		int num1, num2;
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Enter first number: ");
 		num1 = input.nextInt();
-		
+
 		System.out.print("Enter second number: ");
 		num2 = input.nextInt();
-		
-		if(num1 != num2) {
+
+		if (num1 != num2) {
 			System.out.println(num1 + " != " + num2);
 		}
-		if(num1 > num2) {
+		if (num1 > num2) {
 			System.out.println(num1 + " > " + num2);
 		}
-		if(num1 < num2) {
+		if (num1 < num2) {
 			System.out.println(num1 + " < " + num2);
 		}
-		if(num1 == num2) {
+		if (num1 == num2) {
 			System.out.println(num1 + " = " + num2);
 		}
-		
+
 		input.close();
 	}
 
