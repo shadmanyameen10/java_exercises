aa9992ddcd77d28ee91c6a70f26ba269d528bb64
Shadman Yameen <shadmanyameen10@gmail.com>
2024-11-08 20:31:48 -0500

    Added solution for Exercise 41

---
+++ src/exercise41.java
@@ -0,0 +1,16 @@
+//Write a Java program to print the ASCII value of a given character.
+
+import java.util.Scanner;
+
+public class exercise41 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char character = scanner.next().charAt(0);
+
+        int asciiValue = (int) character;
+
+        System.out.println("The ASCII value of " + character + " is :" + asciiValue);
+        scanner.close();
+    }
+}
